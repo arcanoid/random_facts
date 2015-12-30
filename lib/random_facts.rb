b905847831494fa3f545fe6aaf160941c3bbbe3d
Vasilis Kalligas <billkall@gmail.com>
2015-12-31 01:54:56 +0200

    Added proper code for producing random facts per type and of proper size once that is given.
    Pending items
    * Add more data
    * Setup all needed ground work for rspec
    * Add proper spec files
    * Release first version of it

--- lib/random_facts.rb
+++ lib/random_facts.rb
@@ -1,5 +1,45 @@
-require "random_facts/version"
+require 'random_facts/version'
 
 module RandomFacts
-  # Your code goes here...
+  class Generator
+    ROOT = File.expand_path("../..", __FILE__)
+
+    def self.process(options = {})
+      file = File.read("#{ROOT}/data/facts.json")
+      data_hash = JSON.parse(file)
+      random_numbers = []
+      final_data = []
+
+      # if options type is given filter out only the facts that refer to that kind of type
+      if options[:type].present?
+        filtered_data = data_hash['facts'].select { |fact| fact['type'] == options[:type] }
+      else
+        filtered_data = data_hash['facts']
+      end
+
+      # if option size is given try to produce random numbers to select
+      if options[:size].present? && filtered_data.size > 0
+        while random_numbers.size < options[:size]
+          number = rand(filtered_data.size - 1)
+          unless random_numbers.include? number
+            random_numbers << number
+          end
+        end
+
+        random_numbers.each do |index|
+          final_data << filtered_data[index]
+        end
+      else
+        final_data = filtered_data
+      end
+
+      final_data.map { |fact| fact['fact'] }
+    end
+
+    def self.valid_types
+      file = File.read("#{ROOT}/data/facts.json")
+      data_hash = JSON.parse(file)
+      data_hash['facts'].collect { |fact| fact['type'] }.uniq.sort
+    end
+  end
 end
