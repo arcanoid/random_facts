e20b7c3e156f3ed952ca6fdde1ce1daf1f412f8b
Vasilis Kalligas <billkall@gmail.com>
2016-08-04 01:25:46 +0300

    Cleaned it up a bit more and fixed deprecations in specs.

--- spec/lib/random_facts_spec.rb
+++ spec/lib/random_facts_spec.rb
@@ -4,37 +4,37 @@ describe RandomFacts::Generator do
   describe '#process' do
     it 'should respond with full list when no option is given' do
       output = RandomFacts::Generator.process
-      output.should be_kind_of Array
-      output.size.should == 6376
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(3162)
     end
 
     it 'should respond with proper list when size option is given' do
       output = RandomFacts::Generator.process({:size => 10})
-      output.should be_kind_of Array
-      output.size.should == 10
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(10)
     end
 
     it 'should respond with proper list when type option is given' do
       output = RandomFacts::Generator.process({:type => 'animal kingdom'})
-      output.should be_kind_of Array
-      output.size.should == 1399
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(1399)
     end
   end
 
   describe '#sources' do
     it 'should respond with proper list' do
       output = RandomFacts::Generator.sources
-      output.should be_kind_of Array
-      output.size.should == 25
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(25)
     end
   end
 
   describe '#valid_types' do
     it 'should respond with proper list' do
       output = RandomFacts::Generator.valid_types
-      output.should be_kind_of Array
-      output.size.should == 26
-      output.should == [
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(25)
+      expect(output).to eq([
           'animal kingdom',
           'cartoon',
           'customs',
@@ -59,9 +59,8 @@ describe RandomFacts::Generator do
           'sports',
           'statistics',
           'trivia',
-          'tv',
-          'uncategorized'
-      ]
+          'tv'
+      ])
     end
   end
 end
